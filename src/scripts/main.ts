2413ab8e94b4eb07a8ff7084b732965eb7ff8bdc
BijanProgrammer <bijaneisapour@gmail.com>
2024-05-22 16:51:32 +0330

    [ADD] download button

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,11 @@
 import {DownloadManagerService} from '../services/download-manager.service.ts';
 
 async function main(): Promise<void> {
+    const downloadButton = document.querySelector('section#queue > button')!;
+    downloadButton.addEventListener('click', downloadButtonClickHandler, {once: true});
+}
+
+async function downloadButtonClickHandler(): Promise<void> {
     const downloadManager = new DownloadManagerService();
     await downloadManager.startQueue();
 }
